7b58aec094e95ac55e6e8613c65e49144b09acde
Siddhesh <iamsiddhesh22@gmail.com>
2025-06-21 13:42:51 +0530

    get baseurl when its on browser

--- src/constant/envs.ts
+++ src/constant/envs.ts
@@ -3,8 +3,12 @@ const SUPABASE_URL = process.env.SUPABASE_URL!;
 const SUPABASE_ANON_KEY = process.env.SUPABASE_ANON_KEY!;
 const GROQ_API_KEY = process.env.GROQ_API_KEY!;
 const CRON_SECRET = process.env.CRON_SECRET!;
-const NEXT_PUBLIC_API_URL = `${window.location.origin}/api/`;
-
+const NEXT_PUBLIC_API_URL = (() => {
+  if (typeof window !== "undefined") {
+    return `${window.location.origin}/api/`;
+  }
+  return "";
+})();
 export {
   GEMINI_API_KEY,
   NEXT_PUBLIC_API_URL,
