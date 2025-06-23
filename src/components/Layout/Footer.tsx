dc73df81ac6129482e296b38237c092e621746ec
Siddhesh <iamsiddhesh22@gmail.com>
2025-06-24 01:16:29 +0530

    change font size

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -4,9 +4,9 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="border-t border-t-zinc-800 w-full p-4">
-      <p className="text-end">{fullName}</p>
-      <p className="text-end text-zinc-500 text-[12px]">
-        last updated at : 22 may 2001
+      <p className="text-end text-[0.8rem] md:text-[0.85rem]">{fullName}</p>
+      <p className="text-end text-zinc-500 text-[0.75rem]">
+        last updated at : 24 June 2001
       </p>
     </footer>
   );
--- src/components/Skills/SkillCard.tsx
+++ src/components/Skills/SkillCard.tsx
@@ -1,7 +1,7 @@
 import { SkillType } from "@/constant/types";
 import Image from "next/image";
 import React, { useRef } from "react";
-import { motion, useInView } from "framer-motion";
+import { motion, useInView, useScroll, useTransform } from "framer-motion";
 
 type SkillCardProps = React.FC<{
   data: SkillType;
@@ -11,6 +11,11 @@ type SkillCardProps = React.FC<{
 const SkillCard: SkillCardProps = ({ data, index }) => {
   const ref = useRef(null);
   const isInView = useInView(ref, { once: true });
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start", "end"],
+  });
+  const beamHeight = useTransform(scrollYProgress, [0, 1], ["200%", "0%"]);
 
   return (
     <motion.div
@@ -22,21 +27,29 @@ const SkillCard: SkillCardProps = ({ data, index }) => {
         delay: index * 0.05,
         ease: "easeOut",
       }}
-      className="flex h-[150px] w-[110px] rounded-xl flex-col border border-zinc-900 items-center justify-between p-1"
-      whileHover={{
-        scale: 1.02,
-        boxShadow: "inset 0 2px 10px rgba(255, 255, 255, 0.06)",
-      }}
+      className="flex h-[150px] w-[110px] relative rounded-xl flex-col border border-zinc-900 items-center justify-between p-[1px] overflow-hidden"
+      // whileHover={{
+      //   scale: 1.02,
+      //   boxShadow: "inset 0 2px 10px rgba(255, 255, 255, 0.06)",
+      // }}
     >
-      <motion.div
-        className="h-full w-full flex items-center justify-center"
-        initial={{ filter: "grayscale(1)" }}
-        whileHover={{ filter: "grayscale(0)" }}
-        transition={{ duration: 0.3 }}
-      >
-        <Image src={data.img} height={60} width={60} alt={data.name} />
-      </motion.div>
-      <p className="text-center text-[0.75rem] text-zinc-300">{data.name}</p>
+      {/* <motion.span
+        className="absolute top-0 right-0 z-0 left-0 bottom-0 bg-gradient-to-r from-violet-600 to-fuchsia-500"
+        style={{
+          height: beamHeight,
+        }}
+      /> */}
+      <div className="h-full flex flex-col w-full z-10 bg-zinc-950 rounded-lg p-1">
+        <motion.div
+          className="h-full w-full flex items-center justify-center"
+          initial={{ filter: "grayscale(1)" }}
+          whileHover={{ filter: "grayscale(0)" }}
+          transition={{ duration: 0.3 }}
+        >
+          <Image src={data.img} height={60} width={60} alt={data.name} />
+        </motion.div>
+        <p className="text-center text-[0.75rem] text-zinc-300">{data.name}</p>
+      </div>
     </motion.div>
   );
 };
